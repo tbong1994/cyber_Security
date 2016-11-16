913b862f704122fcd2e9f5a7fad7aacb1dee0eae
tbong1994 <youngkeu@buffalo.edu>
2016-11-15 19:31:09 -0500

    parts a and b done. Need to figure out how to keep track of 3 things at the same time efficiently.

--- hw6/youngkeu.py
+++ hw6/youngkeu.py
@@ -7,6 +7,8 @@
 #if ttl is empty, global ttl value is assigned
 
 def unique(file_name):
+	
+	#part a
 	zone_file = open(file_name, "r") #open file
 	
 	#count of unique values.
@@ -16,10 +18,49 @@ def unique(file_name):
 	uniq_type = {}
 	uniq_data = {}
 	
+	#count of different names associated with unique type.
+	diff_name_for_type = {} #pair name:type
+	
 	for line in zone_file:
 		line = line.split() #line is an array with 5 elements now.[name,ttl,class,type,data]
-		#print line
+		if(uniq_name.has_key(line[0])):
+			uniq_name[line[0]] +=1
+		else:
+			uniq_name[line[0]] = 1
+		if(uniq_ttl.has_key(line[1])):
+			uniq_ttl[line[1]] +=1
+		else:
+			uniq_ttl[line[1]] = 1
+			
+		if(uniq_class.has_key(line[2])):
+			uniq_class[line[2]] +=1
+		else:
+			uniq_class[line[2]] = 1
+			
+		if(uniq_type.has_key(line[3])):
+			uniq_type[line[3]] +=1
+		else:
+			uniq_type[line[3]] = 1
+			
+		if(uniq_data.has_key(line[4])):
+			uniq_data[line[4]] +=1
+		else:
+			uniq_data[line[4]] = 1
 		
+	#part b
+	#LENGTH OF EACH DICTIONARY REPRESENTS # OF UNIQUE VALUES.
+	print len(uniq_name)
+	print len(uniq_ttl)
+	print len(uniq_class)
+	print len(uniq_type)
+	print len(uniq_data)
+	
+	#part c
+	#part d. Count # of different names associated with each type.
+	
+	
+	
+	
 	zone_file.close()
 
 #zone file
